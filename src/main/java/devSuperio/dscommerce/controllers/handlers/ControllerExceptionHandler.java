3b81e6c47879517cb9762598d4e71b65809d7e5f
wlad9 <zav8b9@gmail.com>
2023-09-01 19:47:57 -0300

    DTO-CustomError - Classe ControllerExceptionHandler

---
+++ src/main/java/devSuperio/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -0,0 +1,21 @@
+package devSuperio.dscommerce.controllers.handlers;
+
+import devSuperio.dscommerce.dto.CustomError;
+import devSuperio.dscommerce.excepitions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ContollerExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
